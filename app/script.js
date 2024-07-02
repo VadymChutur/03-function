65fa178db1d8ed755b2673b0da2943561c6b9d02
Vadym Chutur <falloutgreck@gmail.com>
2024-07-02 10:27:11 +0300

    upd

--- app/script.js
+++ app/script.js
@@ -2,4 +2,17 @@ function hello() {
   console.log('Hello');
 }
 
-let btn;
+const btn = document.querySelector('#btn'); //knopka
+console.log(btn);
+
+btn.addEventListener('click', hello); //vuzov funkcii po kliku
+
+const modalShowBtn = document.querySelector('#modal-show'); //knopka modalki
+console.log(modalShowBtn);
+
+function showModal() {
+  const modal = document.querySelector('.modal');
+  modal.classList.toggle('modal-show');
+}
+
+modalShowBtn.addEventListener('click', showModal);
